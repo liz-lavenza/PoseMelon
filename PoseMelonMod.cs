bc18b6d7fa1c2e4153bfeb2a05bb324b6489e977
liz-lavenza <madeleine_is_not_my_real_name@protonmail.ch>
2023-07-21 22:12:10 -0500

    Create initial MelonLoader PoseMod injector

---
+++ PoseMelonMod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using MelonLoader;
+
+namespace PoseMelon
+{
+	public class PoseMelonPlugin : MelonPlugin
+	{
+		public override void OnApplicationStarted()
+		{
+			try
+			{
+				string text = File.ReadAllText("./PoseMod/Modules/DetectedVersion.ini");
+				Assembly assembly = Assembly.LoadFile("./PoseMod/Modules/" + text);
+				Type[] types = assembly.GetTypes();
+				foreach (Type type in types)
+				{
+					if (type.IsClass && type.Namespace == "pmcaller" && type.Name == "pmcaller")
+					{
+						MethodInfo method = type.GetMethod("Call");
+						method.Invoke(null, null);
+						break;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				File.AppendAllText("pm_SideInjector error.txt", ex.Message + "\n" + ex.StackTrace);
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PoseMelon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PoseMelon")]
+[assembly: AssemblyCopyright("Copyright © LizLavenza 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7430aba1-b7dd-427b-ba9a-aa01579a3a1f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(PoseMelon.PoseMelonPlugin), "PoseMod MelonLoader Plugin", "0.0.1", "LizLavenza")]
+[assembly: MelonGame("YandereDev", "YandereSimulator")] 
